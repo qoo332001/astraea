381ecafdd9d8c80d297c163b350d508e93bc0994
qoo332001 <sean0651101@gmail.com>
2022-03-28 22:46:41 +0800

    update

--- app/src/main/java/org/astraea/workload/RealtimeApplication.java
+++ app/src/main/java/org/astraea/workload/RealtimeApplication.java
@@ -1,66 +1,52 @@
 package org.astraea.workload;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.Collection;
-import java.util.Set;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.astraea.consumer.Record;
-import org.astraea.utils.DataUnit;
-
-public class TimeRelatedApplication {
-
+import java.time.Duration;
+import java.util.Map;
+public class RealtimeApplication {
   public static class Producer implements Workload {
-
-    @Override
-    public void run(String bootstrapServer, String argument) throws InterruptedException {
-      final String[] allArguments = argument.split(",");
-      final int payloadSize = Integer.parseInt(allArguments[0]);
-      final String[] topicName = allArguments[1].split(":");
-
-      try (var kafkaProducer = org.astraea.producer.Producer.of(bootstrapServer).kafkaProducer()) {
-        while (!Thread.currentThread().isInterrupted()) {
-          var topicToSend = topicName[LocalDateTime.now().getMinute() / 10 % topicName.length];
-          var valueSize =
-              (int) ((ThreadLocalRandom.current().nextGaussian() + 1) / 2 * payloadSize);
-          kafkaProducer.send(new ProducerRecord<>(topicToSend, new byte[valueSize]));
-          System.out.printf("Send %s to %s%n", DataUnit.Byte.of(valueSize), topicToSend);
-          TimeUnit.MILLISECONDS.sleep(
-              (long) (ThreadLocalRandom.current().nextGaussian() + 1) / 2 * 100L);
+    /**
+     * @param argument        topicName:Producers
+     */
+        @Override
+        public void run(String bootstrapServer, String argument) {
+           final String[] split = argument.split(":");
+           var topicName=split[0];
+           final KafkaProducer<byte[], byte[]> kafkaProducer = org.astraea.producer.Producer.of(bootstrapServer).kafkaProducer();
+           while (true)
+                kafkaProducer.send(new ProducerRecord<>(topicName, new byte[10]));
         }
-      }
     }
+    public static class Consumer implements Workload {
+        /**
+         * @param bootstrapServer
+         * @param argument        topicName:Consumers
+         */
+        @Override
+        public void run(String bootstrapServer, String argument) {
+            final String[] split = argument.split(":");
+            var topicName=split[0];
+            final KafkaConsumer<?, ?> kafkaConsumer =
+                    new KafkaConsumer<>(
+                            Map.of(
+                                    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                                    bootstrapServer,
+                                    ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,
+                                    "earliest"));
 
-    @Override
-    public String explainArgument() {
-      return "(data size),[topic 1]:[topic 2]:[topic 3]...";
-    }
-  }
-
-  public static class Consumer implements Workload {
-
-    @Override
-    public void run(String bootstrapServer, String argument) {
-      final String[] all = argument.split(",");
-      final String[] topicName = all[1].split(":");
-
-      try (var build =
-          org.astraea.consumer.Consumer.builder()
-              .brokers(bootstrapServer)
-              .topics(Set.of(topicName))
-              .groupId(all[0])
-              .build()) {
-        while (!Thread.currentThread().isInterrupted()) {
-          final Collection<Record<byte[], byte[]>> poll = build.poll(Duration.ofSeconds(1));
+            while (true){
+                ka
+                kafkaConsumer.poll(Duration.ofSeconds(1L));
+            }
         }
-      }
     }
-
-    @Override
-    public String explainArgument() {
-      return "(consumer group name),[topic 1]:[topic 2]:[topic 3]...";
+    public static void main(String[] args) throws InterruptedException {
+        Workload workloadProducer = new RealtimeApplication.Producer();
+        workloadProducer.run("192.168.103.39:11300", "test-1:10");
+        Workload workloadConsumer = new RealtimeApplication.Producer();
+        workloadConsumer.run("192.168.103.39:11300", "test-1:10");
     }
-  }
 }
